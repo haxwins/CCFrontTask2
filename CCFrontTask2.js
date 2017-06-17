3b7e8af3f06969b76890187a19b3a3878aff4ad8
haxwins <haxregist@gmail.com>
2017-06-17 15:52:15 +0200

    1

--- CCFrontTask2.js
+++ CCFrontTask2.js
@@ -1,11 +1,8 @@
-damage = function (spellString){
-	var sumDmg = 3; //damage of first 'fe' and last 'ai' in spell (1+2) becouse they need to be there for spell to work 
-	//subspells in hierarchy from highest priority to lowest depending which should be counted first and if spell effect other one
-	//'fe' and last 'ai' are executed first becouse we know they must be there plus they cant be interupted by other subspells
-	//'ai' is also here becouse there can by more than one in spell
-	var subSpells = [		
-		{
-			name: "dai",
+exports.damage = function (spellString){
+	var damage = 3; 		//damage of first 'fe' and last 'ai' in spell (1+2) becouse they need to be there for spell to work 
+	var subSpells = [		//subspells in hierarchy from highest priority to lowest depending which should be counted first	
+		{					//'fe' and last 'ai' are executed first becouse we know they must be there plus they cant be interupted by other subspells
+			name: "dai",	//'ai' is also here becouse there can by more than one in spell	
 			dmg: 5
 		},
 		{
@@ -37,20 +34,16 @@ damage = function (spellString){
 	subSpells.map((v) => {
 		while(spellString.indexOf(v.name)!=-1){			//checking if there is subspell in spellString if so replaceing it with '_' symbol and adding damage
 			spellString = spellString.substr(0,spellString.indexOf(v.name)) + "_" + spellString.substr(spellString.indexOf(v.name)+v.name.length);
-			sumDmg+=v.dmg;
+			damage+=v.dmg;
 		}
 	});
-	//decrease damage for character that left in spellString, not counting '_' symbol we used to replace subspells, 
-	//we cant remove subspell and 'glue' the rest becouse that can create new subspell that shouldn't be there so we separate them with '_'
-	for(var i=0;i<spellString.length;i++){		
-		if(spellString[i]!=='_'){
-			sumDmg--;
+	for(var i=0;i<spellString.length;i++){		//decrease damage for character that left in spellString, not counting '_' symbol we used to replace subspells, 
+		if(spellString[i]!=='_'){				//we cant remove subspell and 'glue' the rest becouse that can create new subspell that shouldn't be there so we separate them with '_'
+			damage--;
 		}
 	}
-	if(sumDmg<0)	//checks if output damage is lower than zero if so return 0;
+	if(damage<0)	//checks if output damage is lower than zero if so return 0;
 		return 0;
-	return sumDmg;
+	return damage;
 };
-//testing section
-x=damage("feaineain'");	//spell here
-console.log(x);			//damage output	
+
